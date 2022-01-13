8c129c48c7cde8272ef7d2939f2d6e974813ac47
Sam Wen <samw@ritani.com>
2022-01-13 13:18:45 -0500

    adjust default config

--- default-cfg.js
+++ default-cfg.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-    minima_duration_ms: 250,
-    batch_size: 256,
+    minima_duration_ms: 500,
+    batch_size: 128,
     max_await_time_ms: 1000,
     data_dir: null,
 };
